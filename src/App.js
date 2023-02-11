b90120457874a73cf1a6107274b0944fa45a6a32
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-11 13:16:06 +0200

    created a button that changes the title counter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
-
 function App() {
+  const [counter,setCounter] = useState(0);
+  useEffect(() => {
+    setCounter((prevState) => {
+      const newState = prevState + 1;
+      return newState;
+    })
+    document.title = `Counter(${counter})`
+  },[])
   return (
     <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
+      <section className="hero">
+        <div className="hero-body">
+          <p className="title">Counter({counter})</p>
+          <p className="subtitle">by Boom.dev</p>
         </div>
       </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
+      <div className="container is-fullhd">
+        <div className="notification">
+          <button onClick={() => setCounter(counter + 1)}>text</button>
         </div>
       </div>
     </div>
   );
 }
-
 export default App;
