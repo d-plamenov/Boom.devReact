5e3d46b27b400ede25b7526b22b12cb2caa5ef71
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-11 23:29:13 +0200

    Edited structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Document from "./components/Document";
+import Document from "./Document";
 
 function App() {
   const [text, setText] = useState("");
-  
+
   useEffect(() => {
     fetch("https://jaspervdj.be/lorem-markdownum/markdown.txt")
-    .then((response) => response.text())
-    .then((data) => setText(data));
-  }, [])
-  
+      .then((response) => response.text())
+      .then((data) => setText(data));
+  }, []);
+
   return (
     <div className="App">
-      <Document title="Terms and Conditions" content={text} />
-      {/* <section class="hero">
+      <section class="hero">
         <div class="hero-body">
           <p class="title">A React Task</p>
           <p class="subtitle">by Boom.dev</p>
@@ -22,9 +21,9 @@ function App() {
       </section>
       <div class="container is-fullhd">
         <div class="notification">
-          Edit the <code>./src</code> folder to add components.
+          <Document title="Terms and Conditions" content={text} />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
