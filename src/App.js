3827c4d7b0c45c19882a5aedef9527c6033aa80a
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-11 19:32:51 +0200

    added functionality to cool notepad

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import "./App.css";
+import {useState, useEffect} from "react"
 
 function App() {
+  const [text, setText] = useState("");
+  const handleChange = (event) => {
+    setText((event.target).value);
+  }
+  const handleClear = () => {
+    setText("")
+    localStorage.clear();
+  }
+  const handleSave = () => {
+    localStorage.setItem("noteText", text)
+    // console.log(localStorage.getItem("noteText"))
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("noteText")) {
+      setText(localStorage.getItem("noteText"))
+    }
+    
+  }, [])
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea
+              className="textarea is-large"
+              placeholder="Notes..."
+              onChange={handleChange}
+              value={text}
+            />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" onClick={handleSave}>Save</button>
+        <button className="button is-large" onClick={handleClear}>Clear</button>
       </div>
     </div>
   );
