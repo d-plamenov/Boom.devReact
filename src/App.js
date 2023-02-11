7918ec5d97fde725f204f2e27222103fe37ec8be
Heronstares <diman.p.dimitrov@gmail.com>
2023-02-11 23:09:26 +0200

    added terms and condition to accept

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Document from "./Document";
 
 function App() {
+  const [text, setText] = useState("");
+  
+  useEffect(() => {
+    fetch("https://jaspervdj.be/lorem-markdownum/markdown.txt")
+    .then((response) => response.text())
+    .then((data) => setText(data));
+  }, [])
+  
   return (
     <div className="App">
+      <Document title="Terms and Conditions" content={text} />
       <section class="hero">
         <div class="hero-body">
           <p class="title">A React Task</p>
---
+++ src/Document.js
@@ -0,0 +1,24 @@
+import { Fragment, useState } from "react";
+
+const Document = ({ title, content }) => {
+    const [complete, setComplete] = useState(false)
+    const handleScroll = (e) => {
+        const div = e.target;
+        if (div.scrollTop >= (div.scrollHeight - div.offsetHeight)) {
+            setComplete(true)
+        }
+    }
+    return (
+        <Fragment>
+            <h1 className="title">{title}</h1>
+            <div className="content" onScroll={handleScroll} style={{
+                "overflow": "scroll",
+                "width": "550px",
+                "height": "500px"
+            }}>{content}</div>
+            <button disabled={!complete}>I Agree</button>
+        </Fragment>
+    )
+}
+
+export default Document;
